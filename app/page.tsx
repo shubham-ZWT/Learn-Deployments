4e9867182ba0101a1ac40c78b50fe4b16e67e2dd
shubhamp-zwt <shubham.patel@zealousweb.com>
2026-04-17 17:11:16 +0530

    new feature in staging for testgin

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
         Hello, Next.js!. This is a simple Project for Learning Deployment.
       </h1>
       <p className="text-lg">this is a simple paragraph.</p>
-      <p className="text-md">This para is a new feature for staging </p>
+      <p className="text-md">
+        This para is a new feature for staging for testing.{" "}
+      </p>
     </>
   );
 }
