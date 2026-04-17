8044323321074cd3ef1bee0252b1bfe0449b3565
shubhamp-zwt <shubham.patel@zealousweb.com>
2026-04-17 16:53:03 +0530

    initial project setup to main

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <>h1</>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Hello, Next.js!. This is a simple Project for Learning Deployment.</h1>
+      <p className="text-lg">this is a simple paragraph.</p>
+    </>
+  );
 }
