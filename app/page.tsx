66d3940784e39589616cd4e907d15a0ef7f431a6
shubhamp-zwt <shubham.patel@zealousweb.com>
2026-04-17 16:55:52 +0530

    new feature in staging

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold">Hello, Next.js!. This is a simple Project for Learning Deployment.</h1>
+      <h1 className="text-3xl font-bold">
+        Hello, Next.js!. This is a simple Project for Learning Deployment.
+      </h1>
       <p className="text-lg">this is a simple paragraph.</p>
+      <p className="text-md">This para is a new feature for staging </p>
     </>
   );
 }
